265616afcc22f005d824bb1d94ded37807138160
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2025-05-12 21:44:47 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -17,3 +17,52 @@ for (skill of skills) {
   li.innerText = skill;
   skillsList.appendChild(li);
 }
+
+const messageForm = document.getElementsByName("leave_message")[0];
+
+let messageSection = document.getElementById("messages");
+let messageList = messageSection.querySelector("ul");
+if (messageList.childElementCount === 0) {
+  messageSection.style.display = "none";
+}
+
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let userName = event.target.usersName.value;
+  let userEmail = event.target.usersEmail.value;
+  let userMessage = event.target.usersMessage.value;
+  console.log(userName, userEmail, userMessage);
+  event.target.reset();
+  let messageSection = document.getElementById("messages");
+  let messageList = messageSection.querySelector("ul");
+  let newMessage = document.createElement("li");
+  newMessage.innerHTML = `<a href="mailto: ${userEmail}"> ${userName} </a> <br/> <span>${userMessage}</span> <br/>`;
+
+  let editButton = document.createElement("button");
+  editButton.innerText = "Edit";
+  editButton.type = "button";
+  editButton.addEventListener("click", () => {
+    let entry = editButton.parentNode;
+    let message = entry.querySelector("span");
+    let newMessage = prompt("Edit your message:", message.innerText);
+    if (newMessage) {
+      message.innerText = newMessage;
+    }
+  });
+  newMessage.appendChild(editButton);
+
+  let removeButton = document.createElement("button");
+  removeButton.innerText = "Remove";
+  removeButton.type = "button";
+  removeButton.addEventListener("click", () => {
+    let entry = removeButton.parentNode;
+    entry.remove();
+    if (messageList.childElementCount === 0) {
+      messageSection.style.display = "none";
+    }
+  });
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
+  messageSection.style.display = "block";
+});
